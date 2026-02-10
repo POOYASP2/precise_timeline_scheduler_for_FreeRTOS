eafd2b43ac5e0b386e95b214567fa4b92655a315
Pooya Sharifi <pooyasharifi.sp2@gmail.com>
2026-02-10 18:25:04 +0100

    refactor: standardize UART driver header and implementation formatting

--- drivers/uart.h
+++ drivers/uart.h
@@ -1,13 +1,13 @@
-#ifndef __PRINTF__
-#define __PRINTF__
+#ifndef UART_H
+#define UART_H
 
-#include "FreeRTOS.h"
+#include <stdint.h>
 
-#define UART0_ADDRESS                         ( 0x40004000UL )
-#define UART0_DATA                            ( *( ( ( volatile uint32_t * ) ( UART0_ADDRESS + 0UL ) ) ) )
-#define UART0_STATE                           ( *( ( ( volatile uint32_t * ) ( UART0_ADDRESS + 4UL ) ) ) )
-#define UART0_CTRL                            ( *( ( ( volatile uint32_t * ) ( UART0_ADDRESS + 8UL ) ) ) )
-#define UART0_BAUDDIV                         ( *( ( ( volatile uint32_t * ) ( UART0_ADDRESS + 16UL ) ) ) )
+#define UART0_BASE      (0x40004000UL)
+#define UART0_DATA      (*((volatile uint32_t *)(UART0_BASE + 0x00)))
+#define UART0_STATE     (*((volatile uint32_t *)(UART0_BASE + 0x04)))
+#define UART0_CTRL      (*((volatile uint32_t *)(UART0_BASE + 0x08)))
+#define UART0_BAUDDIV   (*((volatile uint32_t *)(UART0_BASE + 0x10)))
 
 void UART_init(void);
 void UART_printf(const char *s);
